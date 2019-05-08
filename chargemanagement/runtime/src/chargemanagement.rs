4a50695206b5b62a0232cb06ae06342eb7aa8619
Shunki Watanabe <arecchan@gmail.com>
2019-05-08 18:38:15 +0900

    共通部となり得る部分をくくりだした

--- chargemanagement/runtime/src/chargemanagement.rs
+++ chargemanagement/runtime/src/chargemanagement.rs
@@ -54,23 +54,10 @@ decl_module! {
         fn create_car(origin) -> Result {
             let sender = ensure_signed(origin)?;
 
-            // 現在の車の所持数を取得
-            let owned_car_count = Self::owned_car_count(&sender);
-            // 検証しつつあたらしい値を生成
-            let new_owned_car_count = owned_car_count.checked_add(1).ok_or("Overflow adding a new car to account balance")?;
-
-            // 現在のすべての車の数を取得
-            let all_cars_count = Self::all_cars_count();
-            // 検証しつつあたらしい値を生成
-            let new_all_cars_count = all_cars_count.checked_add(1).ok_or("Overflow adding a new car to total supply")?;
-
             // 現在のnonceの取得、IDのハッシュ値のランダム生成
             let nonce = <Nonce<T>>::get();
             let random_hash = (<system::Module<T>>::random_seed(), &sender, nonce)
                 .using_encoded(<T as system::Trait>::Hashing::hash);
-
-            // IDがすでに存在するかどうかの検証
-            ensure!(!<CarOwner<T>>::exists(random_hash), "Car already exists");
 
             // 実際に構造体を生成
             let new_car = Car {
@@ -79,23 +66,47 @@ decl_module! {
                 price: <T::Balance as As<u64>>::sa(0),
             };
 
-            <Cars<T>>::insert(random_hash, new_car);
-            <CarOwner<T>>::insert(random_hash, &sender);
-
-            // 全ての車の情報の更新
-            <AllCarsArray<T>>::insert(all_cars_count, random_hash);
-            <AllCarsCount<T>>::put(new_all_cars_count);
-            <AllCarsIndex<T>>::insert(random_hash, all_cars_count);
-
-            <OwnedCarsArray<T>>::insert((sender.clone(), owned_car_count), random_hash);
-            <OwnedCarsCount<T>>::insert(&sender, new_owned_car_count);
-            <OwnedCarsIndex<T>>::insert(random_hash, owned_car_count);
-
             <Nonce<T>>::mutate(|n| *n += 1);
 
-            Self::deposit_event(RawEvent::Created(sender, random_hash));
+            // ストレージ変数を更新
+            Self::mint(sender, random_hash, new_car)?;
 
             Ok(())
         }
+    }
+}
+
+// プライベート関数
+impl<T: Trait> Module<T> {
+    // Carオブジェクトから新しい作成を作成し、すべてのストレージ変数を更新する
+    fn mint(to: T::AccountId, car_id: T::Hash, new_car: Car<T::Hash, T::Balance>) -> Result {
+        // IDがすでに存在するかどうかの検証
+        ensure!(!<CarOwner<T>>::exists(car_id), "Car already exists");
+
+        // 現在の車の所持数を取得
+        let owned_car_count = Self::owned_car_count(&to);
+        // 検証しつつあたらしい値を生成
+        let new_owned_car_count = owned_car_count.checked_add(1).ok_or("Overflow adding a new car to account balance")?;
+
+        // 現在のすべての車の数を取得
+        let all_cars_count = Self::all_cars_count();
+        // 検証しつつあたらしい値を生成
+        let new_all_cars_count = all_cars_count.checked_add(1).ok_or("Overflow adding a new car to total supply")?;
+
+        <Cars<T>>::insert(car_id, new_car);
+        <CarOwner<T>>::insert(car_id, &to);
+
+        // 全ての車の情報の更新
+        <AllCarsArray<T>>::insert(all_cars_count, car_id);
+        <AllCarsCount<T>>::put(new_all_cars_count);
+        <AllCarsIndex<T>>::insert(car_id, all_cars_count);
+
+        <OwnedCarsArray<T>>::insert((to.clone(), owned_car_count), car_id);
+        <OwnedCarsCount<T>>::insert(&to, new_owned_car_count);
+        <OwnedCarsIndex<T>>::insert(car_id, owned_car_count);
+
+        Self::deposit_event(RawEvent::Created(to, car_id));
+
+        Ok(())
     }
 }
