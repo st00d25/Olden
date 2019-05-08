da388a5a2f9ce2552de1bcb20ebb72bf5aa0fe52
Shunki Watanabe <arecchan@gmail.com>
2019-05-08 12:43:57 +0900

    構造体を保存できるように

--- chargemanagement/runtime/src/chargemanagement.rs
+++ chargemanagement/runtime/src/chargemanagement.rs
@@ -1,20 +1,34 @@
 use support::{decl_storage, decl_module, StorageMap, StorageValue, dispatch::Result};
 use system::ensure_signed;
+use runtime_primitives::traits::{As, Hash};
+use parity_codec::{Encode, Decode};
 
-pub trait Trait: system::Trait {}
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub struct Car<Hash, Balance> {
+    id: Hash,
+    price: Balance,
+}
+
+pub trait Trait: balances::Trait {}
 
 decl_storage! {
     trait Store for Module<T: Trait> as ChargeManagementStorage {
-          Value: map T::AccountId => u64;
+          OwnedCar: map T::AccountId => Car<T::Hash, T::Balance>;
   }
 }
 
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        fn set_value(origin, value: u64) -> Result {
+        fn create_car(origin) -> Result {
             let sender = ensure_signed(origin)?;
 
-            <Value<T>>::insert(sender, value);
+            let new_car = Car {
+                id: <T as system::Trait>::Hashing::hash_of(&0),
+                price: <T::Balance as As<u64>>::sa(0),
+            };
+
+            <OwnedCar<T>>::insert(&sender, new_car);
 
             Ok(())
         }
