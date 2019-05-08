d17e6fdf45d7803c2bed23f70d6bb6a14755386c
Shunki Watanabe <arecchan@gmail.com>
2019-05-08 17:38:34 +0900

    トラッキングに対応

--- chargemanagement/runtime/src/chargemanagement.rs
+++ chargemanagement/runtime/src/chargemanagement.rs
@@ -28,6 +28,14 @@ decl_storage! {
     trait Store for Module<T: Trait> as ChargeManagementStorage {
         Cars get(cars): map T::Hash => Car<T::Hash, T::Balance>;
         CarOwner get(owner_of): map T::Hash => Option<T::AccountId>;
+
+        // 全体に対する情報
+        AllCarsArray get(car_by_index): map u64 => T::Hash;
+        AllCarsCount get(all_cars_count): u64;
+        // 削除時に最後の要素と削除したい要素を入れ替えて穴が開かないようにする
+        // O(1)で操作するためにインデックスを覚えておくようにする
+        AllCarsIndex: map T::Hash => u64;
+
         OwnedCar get(car_ofowner): map T::AccountId => T::Hash;
 
         Nonce:u64;
@@ -39,15 +47,24 @@ decl_module! {
 
         fn deposit_event<T>() = default;
 
+       // 車の生成
         fn create_car(origin) -> Result {
             let sender = ensure_signed(origin)?;
 
+            let all_cars_count = Self::all_cars_count();
+
+            // 追加時の検証
+            let new_all_cars_count = all_cars_count.checked_add(1).ok_or("Overflow adding a new car to total supply")?;
+
+            // 現在のnonceの取得、IDのハッシュ値のランダム生成
             let nonce = <Nonce<T>>::get();
             let random_hash = (<system::Module<T>>::random_seed(), &sender, nonce)
                 .using_encoded(<T as system::Trait>::Hashing::hash);
 
+            // IDがすでに存在するかどうかの検証
             ensure!(!<CarOwner<T>>::exists(random_hash), "Car already exists");
 
+            // 実際に構造体を生成
             let new_car = Car {
                 id: <T as system::Trait>::Hashing::hash_of(&0),
                 price: <T::Balance as As<u64>>::sa(0),
@@ -55,9 +72,17 @@ decl_module! {
 
             <Cars<T>>::insert(random_hash, new_car);
             <CarOwner<T>>::insert(random_hash, &sender);
+
+            // 全ての車の情報の更新
+            <AllCarsArray<T>>::insert(all_cars_count, random_hash);
+            <AllCarsCount<T>>::put(new_all_cars_count);
+            <AllCarsIndex<T>>::insert(random_hash, all_cars_count);
+
             <OwnedCar<T>>::insert(&sender, random_hash);
 
             <Nonce<T>>::mutate(|n| *n += 1);
+
+            Self::deposit_event(RawEvent::Created(sender, random_hash));
 
             Ok(())
         }
